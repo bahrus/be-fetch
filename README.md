c59c3adb329f5de45855b117b7121d429d582a8c
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2024-03-30 15:10:08 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # be-fetch
+
+*be-fetch* turns the tag it adorns into a web component that inherits from [fetch-for](https://github.com/bahrus/fetch-for).
+
+```html
+<medical-prescriptions
+    enh-be-fetch 
+    onerror
+    href="https://my-website.com/prescriptions/patient/zero">
+<medical-prescriptions>
+
+
+...
+<medical-prescriptions
+>
+</medical-prescriptions>
+```
+
+## Using a custom web component to extend. [Untested]
+
+The *fetch-for* web component is a fairly non-opinionated web component.  But often times any particular app will want to make particular choices as far as how to define the base url for all the fetch requests, credentials, JWT headers, etc.  fetch-for provides [many small methods](https://github.com/bahrus/fetch-for/blob/baseline/fetch-for.ts) that can be overridden to allow this to be customized according to such needs.
+
+Such app's can define their own web component, most likely extending fetch-for.
+
+be-fetch can be instructed to use this custom web component definition, instead of the default fetch-for, via two alternate ways (or combine as needs warrant):
+
+### Approach 1 (DRY)
+
+Somewhere in the document (probably ideally within the head tag at the top), add a "link" tag (or any other tag really) with id be-fetch, and attribute data-inherits.  For example:
+
+```html
+<html>
+    <head>
+        <link rel=modulepreload id=be-fetch data-inherits=my-custom-base-fetch-element href=https://myapp.com/resources/be-fetch.js >
+    </head>
+</html>
+```
+
+### Approach 2 (Highly customizable)
+
+specify the custom element name to inherit from within the adorned tag itself:
+
+
+```html
+<medical-prescriptions zero=name
+    enh-be-fetch 
+    inherits=my-custom-base-fetch-element
+    onerror
+    href="https://my-website.com/prescriptions/patient/zero">
+<medical-prescriptions>
+```
---
+++ be-fetch.ts
@@ -0,0 +1,58 @@
+import { BE, propDefaults, propInfo} from 'be-enhanced/BE.js';
+import { BEConfig} from 'be-enhanced/types';
+import { XE} from 'xtal-element/XE.js';
+import { Actions, AllProps, AP, PAP, ProPAP, POA} from './types';
+
+export class BeFetch extends BE<AP, Actions> implements Actions{
+    static override get beConfig(){
+        return {
+            parse: true,
+            //parseAndCamelize: true,
+            isParsedProp: 'isParsed'
+        } as BEConfig;
+    }
+
+    async register(self: this): ProPAP {
+        //TODO:  this seems like a nice shareable function that could be used in other scenarios.
+        //move to trans-render?
+        const {enhancedElement} = self;
+        const {localName} = enhancedElement;
+        const inherits = enhancedElement.getAttribute('inherits') || (<any>window)['be-fetch']?.dataset?.inherits;
+        if(inherits){
+            const inheritFrom = await customElements.whenDefined(inherits);
+            customElements.define(localName, class extends inheritFrom{});
+        }else{
+            if(customElements.get(localName) === undefined){
+                await import('fetch-for/fetch-for.js');
+                const fetchFor = await customElements.whenDefined('fetch-for');
+                customElements.define(localName, class extends fetchFor{});
+            }
+        }
+
+        
+        return{
+            resolved: true,
+        }
+    }
+}
+
+export interface BeKvetching extends AllProps{}
+
+export const tagName = 'be-fetch';
+
+const xe = new XE<AP, Actions>({
+    config:{
+        tagName,
+        isEnh: true,
+        propDefaults:{
+            ...propDefaults,
+        },
+        propInfo: {
+            ...propInfo,
+        },
+        actions:{
+            register: 'isParsed',
+        }
+    },
+    superclass: BeFetch,
+});
